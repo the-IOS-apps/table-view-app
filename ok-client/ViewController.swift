d7601b4b663615b3a1947d68694e931181b2e77c
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-05-20 17:55:32 +0300

    Initial Commit

---
+++ ok-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ok-client
+//
+//  Created by the-teacher on 20/05/15.
+//  Copyright (c) 2015 the-teacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
