e75240bf1c2c4069b0af51557e919cdfb7d559d3
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-05-22 17:16:19 +0300

    cleanup code

--- ok-client/RecipesTableController.swift
+++ ok-client/RecipesTableController.swift
@@ -9,23 +9,20 @@
 import UIKit
 
 class RecipesTableController: UITableViewController, UITextFieldDelegate {
-  
-    func log(let str:String?){ println(str) }
-    
     // MARK: - VARIABLES
-    
     var recipes = [[Recipe]]()
+
     var searchText:String? = "cupcake" {
         didSet {
-            log("VAR: search text changed")
-            
+            log("VAR: searchText changed to \(searchText!)")
+
             // Update text input
             // Just to be ensured
             searchTextField?.text = searchText
-            
+
             // Cleanup Array
             recipes.removeAll()
-            
+
             // Rebuild Array
             refresh()
 
@@ -33,104 +30,47 @@ class RecipesTableController: UITableViewController, UITextFieldDelegate {
             tableView.reloadData()
         }
     }
-    
+
+    // something like settings
     private struct Storyboard {
         static let CellReuseIndex = "Recipe"
     }
 
     // MARK: - OUTLETS
-    
-    
-    
+
     @IBOutlet weak var searchTextField: UITextField! {
         didSet{
             log("UI field is set/ DELEGATE")
 
             // link with this Controller for input processing with `textFieldShouldReturn`
             searchTextField.delegate = self
-            
+
             // set default value
             searchTextField.text = searchText
         }
     }
-    
-    // on keyboard will be appeared
-    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
-        log("textFieldShouldBeginEditing")
-        return true
-    }
-    
-    // on keyboard is appeared
-    func textFieldDidBeginEditing(textField: UITextField) {
-        log("textFieldDidBeginEditing")
-    }
-    
-    // Q: is it the just place to process all text fields of this Controllers?
-    // hm!
-    // Event `on enter` for input field
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        log("==============================================")
-        log("textFieldShouldReturn: UI field some action")
-        
-        if textField == searchTextField {
-            // it's just close keyboard
-            textField.resignFirstResponder()
-            
-            // set new value of variable
-            searchText = textField.text
-        }
 
-        return true
-    }
-    
-    // Custom input field event `on change`
-    func textFieldDidChange(textField: UITextField) {
-        log("text field did chenged!")
-        
-        if textField == searchTextField {
-            // set new value of variable
-            searchText = textField.text
-        }
-    }
-    
-    // MARK: - ACTIONS
-
-    // MARK: - COMMON FUNCTIONS
-
-    func refresh() {
-        log("REFRESH: build and set recipes")
-        
-        for index in 1...15 {
-            var prefix = searchText!
-            var recipe = Recipe(
-                title:   "\(prefix) Recipe \(index)",
-                intro:   "\(prefix) Recipe Intro \(index)",
-                content: "\(prefix) Recipe COntent \(index)"
-            )
-            
-            recipes.append([recipe])
-        }
-    }
-    
     // MARK: - LIFE-CICLE
-    
+
+    // After instantiation and outlet-setting
     override func viewDidLoad() {
-        super.viewDidLoad()
         log("viewDidLoad")
+
+        super.viewDidLoad()
         refresh()
-        
-        // Q: where and when outlets are available?
+
+        // Create custom text field event
         searchTextField.addTarget(self, action: "textFieldDidChange:", forControlEvents: UIControlEvents.EditingChanged)
-        
+
         // ******************************************
-        
+
         // main_queue = dispatch_get_main_queue()
         // closure = { ... }
         // dispatch_async(main_queue, closure)
 
         // Method for reloading TableView
         // self.tableView.reloadData()
-        
+
         // ******************************************
 
         // Uncomment the following line to preserve selection between presentations
@@ -140,39 +80,92 @@ class RecipesTableController: UITableViewController, UITextFieldDelegate {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - TEXT INPUT EVENTS
+
+    // on Enter pressed
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        log("TEXT INPUT: => ENTER")
+
+        if textField == searchTextField {
+            // it's just close keyboard
+            textField.resignFirstResponder()
+
+            // set new value of variable
+            searchText = textField.text
+        }
+
+        return true
     }
 
-    // MARK: - TableVewDataSource
+    // on before keyboard appears
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        log("TEXT INPUT: => BEFORE KEYBOARD APPEARS")
+        return true
+    }
+
+    // on after keyboard appears
+    func textFieldDidBeginEditing(textField: UITextField) {
+        log("TEXT INPUT: => AFTRE KEYBOARD APPEARS")
+    }
+
+    // on Change
+    // * custom Input Event
+    func textFieldDidChange(textField: UITextField) {
+        log("TEXT INPUT: => ON CHANGE")
+
+        if textField == searchTextField {
+            // set new value of variable
+            searchText = textField.text
+        }
+    }
+
+    // MARK: - COMMON FUNCTIONS
+
+    func refresh() {
+        log("REFRESH: build and set recipes")
+
+        for index in 1...15 {
+            var prefix = searchText!
+            var recipe = Recipe(
+                title:   "\(prefix) Recipe \(index)",
+                intro:   "\(prefix) Recipe Intro \(index)",
+                content: "\(prefix) Recipe COntent \(index)"
+            )
+
+            recipes.append([recipe])
+        }
+    }
+
+    // MARK: - TABE VIEW IMPLEMENTATION
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        log("Table SECTIONS calcualtion")
+        log("TableView: SECTIONS calcualtion")
         return recipes.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        log("Table ROWS in SECTION count calcualtion")
+        log("TableView: ROWS in SECTION calcualtion")
         return recipes[section].count
     }
 
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        log("TableView: CELL building")
+
         let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIndex, forIndexPath: indexPath) as! UITableViewCell
 
         // Configure the cell...
         let recipe = recipes[indexPath.section][indexPath.row]
-        cell.textLabel?.text = recipe.title
+
+        cell.textLabel?.text       = recipe.title
         cell.detailTextLabel?.text = recipe.intro
 
-        log("CELL building")
         return cell
     }
+
+    // override func didReceiveMemoryWarning() {
+    //     super.didReceiveMemoryWarning()
+    //     // Dispose of any resources that can be recreated
+    // }
 
     /*
     // Override to support conditional editing of the table view.
@@ -190,7 +183,7 @@ class RecipesTableController: UITableViewController, UITextFieldDelegate {
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
@@ -218,5 +211,4 @@ class RecipesTableController: UITableViewController, UITextFieldDelegate {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
---
+++ ok-client/log_method.swift
@@ -0,0 +1,11 @@
+//
+//  log_method.swift
+//  ok-client
+//
+//  Created by the-teacher on 22/05/15.
+//  Copyright (c) 2015 the-teacher. All rights reserved.
+//
+
+import Foundation
+
+func log(let str:String?){ println(str) }
